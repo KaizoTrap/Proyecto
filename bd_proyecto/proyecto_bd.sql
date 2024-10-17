687a77f8d7121a2e620247f64e4f304266d9eb3c
S_Franco <101674377+KaizoTrap@users.noreply.github.com>
2024-10-17 18:54:23 -0500

    proyecto_bd.sql

--- bd_proyecto/proyecto_bd.sql
+++ bd_proyecto/proyecto_bd.sql
@@ -2,8 +2,47 @@ create database tienda_usb;
 
 use tienda_usb;
 
-CREATE TABLE cliente (
-	id_cliente INT auto_increment primary key,
-    usuario varchar(10),
-    contrasena varchar(45)
+CREATE TABLE usuario (
+	id_usuario INT auto_increment primary key,
+    usuario varchar(10) not null,
+    contrasena varchar(45) not null
+);    
+
+CREATE TABLE datos_usuario (
+	id_dato INT auto_increment primary key,
+    nombre_usuario varchar(45) not null,
+    apellido_usuario varchar(45) not null,
+    correo_usuario varchar(45) not null,
+	numero_identificacion int(15) not null
+);    
+
+CREATE TABLE producto (
+	id_producto INT auto_increment primary key,
+    nombre_producto varchar(45) not null,
+    precio_producto int(10) not null,
+    imagen varchar(45) NULL
+);    
+
+CREATE TABLE decripcion_producto (
+	id_descripcion_producto INT auto_increment primary key,
+    detalle_producto varchar(300) not null,
+    talla varchar(2) null,
+    stock_prenda int NULL,
+    tipo_prenda varchar(45) not null
+);    
+
+CREATE TABLE pedido (
+	id_pedido INT auto_increment primary key,
+    fecha_pedido datetime not null
+);    
+
+CREATE TABLE detalle_pedido (
+	id_detalle_pedido INT auto_increment primary key,
+    cantidad int not null,
+    total int(10) not null
+);    
+
+CREATE TABLE facturas_generadas (
+	id_facturas INT auto_increment primary key,
+    factuas_generadas varchar(45) NULL
 );    
